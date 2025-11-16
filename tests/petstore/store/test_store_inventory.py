da83c946a04e3999792700f1eb666b87b3807ff4
ukk0 <sami.ukk91@gmail.com>
2025-11-16 13:54:09 +0100

    test store inventory

--- tests/petstore/store/test_store_inventory.py
+++ tests/petstore/store/test_store_inventory.py
@@ -1,3 +1,16 @@
-def test_get_inventory_returns_valid_counts():
-    """GET /store/inventory, should return dict of statuses with counts."""
-    pass
+import pytest
+
+
+@pytest.mark.smoke
+@pytest.mark.regression
+def test_get_inventory_returns_valid_counts(petstore_client):
+    """
+    GET /store/inventory, should return dict of statuses with counts.
+    """
+    response = petstore_client.get_store_inventory()
+    assert response.status_code == 200
+
+    response_json = response.json()
+    assert isinstance(response_json["available"], int)
+    assert isinstance(response_json["pending"], int)
+    assert isinstance(response_json["sold"], int)
